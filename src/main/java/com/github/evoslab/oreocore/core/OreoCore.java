ba7149bbc95a36fa49f350a71ddcfe0371692924
StevenPlayzzCoder <65874635+StevenPlayzzCoder@users.noreply.github.com>
2021-01-28 10:50:46 -0500

    this should work...

--- src/main/java/com/github/evoslab/oreocore/core/OreoCore.java
+++ src/main/java/com/github/evoslab/oreocore/core/OreoCore.java
@@ -1,8 +1,5 @@
 package com.github.evoslab.oreocore.core;
 
-import com.github.evoslab.oreocore.core.config.ModConfig;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +8,6 @@ public class OreoCore implements ModInitializer {
 
     public static final String MODID = "oreocore";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
-    public static final ModConfig CONFIG = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new).getConfig();
 
     @Override
     public void onInitialize() {
